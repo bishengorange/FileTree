1a0e9c200f9d0d13f6f5324f4eb90db4cb2edd12
bisheng <138598399+bishengorange@users.noreply.github.com>
2025-03-30 17:02:23 +0800

    Update FileTree.R

--- R/FileTree.R
+++ R/FileTree.R
@@ -44,7 +44,7 @@ FileTree <- function(Path = ".", Prefix = "", Size = TRUE, Depth = Inf,
 
   Red    <- crayon::make_style("#F1441D")
   Orange <- crayon::make_style("#FFA500")
-  Yellow <- crayon::make_style("#FBDA41")
+  Yellow <- crayon::make_style("#F7D94C")
   Green  <- crayon::make_style("#20894D")
   Cyan   <- crayon::make_style("#22A2C3")
   Blue   <- crayon::make_style("#2775B6")
